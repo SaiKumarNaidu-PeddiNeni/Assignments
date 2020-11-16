0036c07c3f653a9a85f6bc243f42f8806cfeef44
SaiKumarNaidu-PeddiNeni <68958425+SaiKumarNaidu-PeddiNeni@users.noreply.github.com>
2020-11-16 17:36:28 +0530

    Add files via upload

--- Assignment1/src/evennumbersquaresum/EvenNumberSquareSum.java
+++ Assignment1/src/evennumbersquaresum/EvenNumberSquareSum.java
@@ -1,10 +1,24 @@
 package evennumbersquaresum;
 
+import java.util.Scanner;
+
 public class EvenNumberSquareSum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner scan = new Scanner(System.in);
+
+		int number = scan.nextInt();
+		int result = 0;
+		while (number > 0) {
+			int value = number % 10;
+			if (value % 2 == 0) {
+				result = result + value * value;
+			}
+			number = number / 10;
+		}
+		System.out.println(result);
+		scan.close();
 	}
 
 }
